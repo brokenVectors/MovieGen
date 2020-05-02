7cc3a408591a13c5c8173c2847a85d28c8c33b2b
brokenVectors <brokenvectors@gmail.com>
2020-05-02 19:47:42 +0200

    Added JSON file and fixed some bugs

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 # Import everything needed to edit video clips
 from moviepy.editor import *
-titles = [
-    ["Hello there", 3],
-    ["My name is...", 3],
-    ["Johna Chord", 3]
-]
+import json
+# titles = [
+#    ["Hello there", 3],
+#    ["Today we will be making a sword in Blender", 3],
+#    ["First we need to make the handle", 3],
+#   ["Extrude it up a bit.", 3],
+#]
+with open('edit_data.json', 'r') as myfile:
+    jsonData=myfile.read()
+
+editData = json.loads(jsonData)
+titles = editData["titles"]
 originalClip = VideoFileClip("toEdit.mp4")
 textClips = []
 timePassed = 0
-for title in titles:
-    txt = title[0]
-    duration = title[1]
+for title in titles.keys():
 
-    txt_clip = TextClip(txt, fontsize=70, color='white')
+    duration = titles[title]
+
+    txt_clip = TextClip(title, fontsize=70, color='white', size=originalClip.size)
     txt_clip = txt_clip.set_pos('center').set_duration(duration).set_start(timePassed)
 
     textClips.append(txt_clip)
-    print("Title: " + txt)
+    print("Title: " + title)
     print("Duration: " + str(duration))
     timePassed += duration
 
